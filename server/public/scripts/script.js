7c567dcd7c683c6cd4f075499dd6af858e1154f1
Adam Gothmann <adamgothmann@Adams-MacBook-Pro.local>
2016-07-12 10:12:25 -0500

    mvp

--- server/public/scripts/script.js
+++ server/public/scripts/script.js
@@ -10,25 +10,32 @@ myApp.controller('playerController', ['$scope', '$http', function($scope, $http)
        url: '/getPlayers'
      }).then( function( response ){
        $scope.allPlayers = response.data;
-       console.log( $scope.allPlayers );
+       console.log("allPlayers from GET: ", $scope.allPlayers );
      });
+     $scope.leftArray.splice(0, $scope.leftArray.length);
+     $scope.myTeam.splice(0, $scope.myTeam.length);
   };//end getPlayers
 
   $scope.movePlayer = function(array, index){
-    $scope.leftArray.push(array.splice(index, 1));
-    console.log($scope.leftArray);
-
-  };//end removePlayer
+    $scope.leftArray.push(array.splice(index, 1).pop());
+    console.log("from leftArray: ", $scope.leftArray);
+  };//end movePlayer
 
   $scope.removePlayer = function(array, index){
-    console.log("in removeplayer");
+    console.log("in removeplayer: ", $scope.leftArray);
     $scope.leftArray.splice(index, 1);
-    console.log($scope.leftArray);
-  };
+  };//end removePlayer
+
   $scope.draftPlayer = function(array, index){
-    console.log("in draftPlayer");
-    $scope.myTeam.push(array.splice(index, 1));
-    console.log($scope.myTeam);
-  };
+    console.log("in draftPlayer", $scope.myTeam);
+    $scope.myTeam.push(array.splice(index, 1).pop());
+  };//end draftPlayer
+
+  $scope.importPlayers = function(array){
+    for(var i = 0; i<200; i++){
+    $scope.leftArray.push(array.splice(0, 1).pop());
+  }
+    console.log("leftArray from importPlayers", $scope.leftArray);
+  };//end importPlayers
   $scope.getPlayers();//displays players on page load
 }]);
